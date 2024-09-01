32888630b2b95a706e21293a38ba293ff2c2201c
BORISLAV-PC\borislav <boris.nedelchev@blueberrysystems.co.uk>
2024-09-01 14:21:32 +0300

    #Increase posts expire time

--- Domain/Categories/Category.cs
+++ Domain/Categories/Category.cs
@@ -6,6 +6,8 @@ namespace Domain.Categories
 {
     public class Category : BaseEntity
     {
+        const int EXPIRE_TIME = 10;
+
         public DateTime ExpireTime { get; set; }
         public string WordPress_Id { get; set; } = "";
         public string Name { get; set; } = "";
@@ -25,7 +27,7 @@ namespace Domain.Categories
         
         public void UpdateExpireTime()
         {
-            ExpireTime = DateTime.UtcNow.AddSeconds(5);
+            ExpireTime = DateTime.UtcNow.AddMinutes(EXPIRE_TIME);
         }
 
         public bool IsEqual(Category other)

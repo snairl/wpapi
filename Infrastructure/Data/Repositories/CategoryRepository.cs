c0b1314d6993a62fc75ddbc7dd20ce9a37192375
BORISLAV-PC\borislav <boris.nedelchev@blueberrysystems.co.uk>
2024-09-05 12:32:40 +0300

    +Add Infrastructure files

---
+++ Infrastructure/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Categories;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IMongoCollection<Category> collection) : base(collection)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Users;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(IMongoCollection<User> collection) : base(collection)
+        {
+        }
+    }
+}
